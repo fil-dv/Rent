0adf0c187965ed83748c462d9ef641f1e7904d63
fil-eadr <d.filonenko@eadr.com.ua>
2016-07-21 13:35:58 +0300

    Add CartController
    Methods:  AddToCart, RemoveFromCart, Index. And view for Index

---
+++ Landlord/WebUI/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models.CustomModels;
+
+namespace WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        IAreaRepository _repoArea;
+
+        public CartController(IAreaRepository repository)
+        {
+            _repoArea = repository;
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        public RedirectToRouteResult AddToCart(int areaId, string returnUrl)
+        {
+            Area area = _repoArea.Areas.FirstOrDefault(a => a.AreaID == areaId);
+            if (area != null)
+            {
+                GetCart().AddItem(area);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Area ar, string returnUrl)
+        {
+            Area area = _repoArea.Areas.FirstOrDefault(a => a.AreaID == ar.AreaID);
+            if (area != null)
+            {
+                GetCart().RemoveLine(area);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+    }
+}
---
+++ Landlord/WebUI/Models/CustomModels/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models.CustomModels
+{
+    public class CartViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }     
+    }
+}
