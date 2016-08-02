2638babdaf0e125fe4c2850e7c8210153502d391
fil-eadr <d.filonenko@eadr.com.ua>
2016-08-02 18:00:20 +0300

    Add MapController, dont work yet

---
+++ Landlord/WebUI/Controllers/MapController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class MapController : Controller
+    {       
+        IAreaRepository _repoArea;
+
+        public MapController(IAreaRepository repository)
+        {
+            _repoArea = repository;
+        }
+
+        public ViewResult ShowOnMap(int areaID)
+        {
+            Area area = _repoArea.Areas.FirstOrDefault(a => a.AreaID == areaID);
+            return View(area);
+        }
+        
+        
+        
+    }
+}
